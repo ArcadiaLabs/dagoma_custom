d4c7b72a0c724596e43e52d5daf8a54de3cf365d
CaptainStouf <stouf@hardware-libre.org>
2016-02-15 17:19:19 +0100

    Signed-off-by: CaptainStouf <stouf@hardware-libre.org>

---
+++ firmwares/New CM-sonde104GT2+4.7k+inductif JAUNE-corrigee/Marlin/Marlin.ino
@@ -0,0 +1,22 @@
+
+#include "Configuration.h"
+#include "pins.h"
+
+
+/*     !!!!!!!!!!!!!!!!!!!!
+
+ligne de commande a changer dans Configuration.h en fonction du palpeur
+
+ligne n 141
+
+nommé
+const bool Z_MIN_ENDSTOP_INVERTING = false; // palpeur banggood orange=False / palpeur blanc= true
+
+
+changer le false en true ou inversement
+
+palpeur orange = false
+palpeur blanc 4 ou 6mm = true
+palpeur bleu = NON FONCTIONNEL
+palpeur vert = true
+*/
